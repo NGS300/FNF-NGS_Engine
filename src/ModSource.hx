bdb73bdeae0833809e1e59d03aa1c109c66f13f4
NGS300 <isaacsilva321tf@gmail.com>
2022-07-25 16:53:57 -0300

    Future

---
+++ src/ModSource.hx
@@ -0,0 +1,10 @@
+class ModSource extends MusicBeatState{
+    public static function onPlayStateCreate(){ // PlayState Create Events
+    }
+    public static function onPlayStateUpdate(){ // PlayState Update Events
+    }
+    public static function onPlayStepEvents(){ // PlayStepEvents/StepEvents
+    }
+    public static function onPlayBeatEvents(){ // PlayBeatEvents/BeatEvents
+    }
+}
